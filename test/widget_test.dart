027133662aabbaa4e5be8cbe2abed8a742d604a7
deepak <deepakpitta2001@gmail.com>
2025-09-22 17:10:31 +0530

    changes are added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:newdemoapp/main.dart';
+import 'package:sweetyai_learning_assistant/main.dart' show MyApp;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
